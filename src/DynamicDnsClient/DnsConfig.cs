9b840ca5fd63c33c702ef38f74d3cbe1ed1949e8
Jarle Nygård <jarle.nygaard@outlook.com>
2021-03-26 14:53:39 +0100

    Init add

---
+++ src/DynamicDnsClient/DnsConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace DynamicDnsClient
+{
+
+    public class DnsConfig
+    {
+        public string SubscriptionId { get; set; }
+        public string ResourceGroupName { get; set; }
+        public string ZoneName { get; set; }
+        public string[] RecordSetNames { get; set; }
+    }
+
+}
